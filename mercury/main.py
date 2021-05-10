bac55bdb7c9f013d9461e406cb6b67f99ee3b4e2
damishra <dxm2269@rit.edu>
2021-05-10 07:23:03 -0400

    mistakes were made

--- mercury/main.py
+++ mercury/main.py
@@ -1,8 +1,15 @@
+from mercury.helpers.path import BASEURL
 from fastapi import FastAPI
+from starlette.responses import JSONResponse, RedirectResponse
 from mercury.routers import auth, users, surveys
 
 
 app = FastAPI()
+
+
+@app.get('/')
+async def root():
+    return RedirectResponse(url="/docs")
 
 app.include_router(auth.router, tags=['auth'])
 app.include_router(users.router, tags=['users'])
